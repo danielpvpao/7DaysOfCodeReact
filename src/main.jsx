981d3067787fb7e1ad7135c7125b6dd814f21ed0
Daniel <danielpvpao@gmail.com>
2024-07-16 12:36:15 -0300

    removendo alterações

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './assets_&_styles/index.css';
-import App from './layouts/App.jsx';
-import Home from './pages/Home.jsx';
-import NotFound from './pages/NotFound.jsx';
-import Signin from './pages/Signin.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './layouts/App.jsx'
+import './assets_&_styles/index.css'
 
+import { createBrowserRouter, RouterProvider}from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import NotFound from './pages/NotFound.jsx'
+import Signin from './pages/Signin.jsx'
+  const router = createBrowserRouter([{
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "NotFound",
+    element: <NotFound />
+  },
+  {
+    path: "Signin",
+    element: <Signin />
+  }
+  ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/signin" component={Signin} />
-        <Route path="*" component={NotFound} />
-      </Switch>
-    </Router>
-  </React.StrictMode>
-);
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
